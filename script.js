53be16036f2b1d5a8c0bb942b6eb887160a48b8a
kermit404 <68835493+kermit404@users.noreply.github.com>
2024-02-26 17:59:42 -0800

    yippee

--- script.js
+++ script.js
@@ -29,3 +29,22 @@ function lightswitch() {
         }
     }
 }
+var clicks = 0;
+var width = 98;
+var height = 118;
+function showhide() {
+    clicks += 1;
+    yippee = document.getElementById("tbh");
+    if (getComputedStyle(yippee).opacity == 1 ) {
+        yippee.style.opacity = 0;
+    }
+    else {
+        yippee.style.opacity = 1;
+    }
+    if (clicks >= 10) {
+        width -= 1;
+        height -= 1.204;
+        yippee.style.width = width + 'px';
+        yippee.style.height = height + 'px';
+    }
+}
